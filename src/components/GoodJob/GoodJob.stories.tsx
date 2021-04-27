e9985152ad2d4a0ebf0c7e704c1f78823ecea99e
Iron <jungjuseong@gmail.com>
2021-04-27 10:28:09 +0900

    add goodjob

---
+++ src/components/GoodJob/GoodJob.stories.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { Story } from '@storybook/react';
+import { css } from "@emotion/react";
+
+import {GoodJob, GoodJobProps} from './GoodJob';
+
+import Icon from '../Icon/Icon';
+
+export default {
+  title: 'Components/GoodJob',
+  component: GoodJob,
+};
+
+const Template: Story<GoodJobProps> = (args) => 
+  <GoodJob {...args}/>
+
+export const goodJob = Template.bind({});
+goodJob.args = {
+  view: true,
+  delay: 800,
+  onComplete: () => console.log('onComplete'),
+
+};
+
+const buttonWrapper = css`
+  .description {
+    margin-bottom: 0.5rem;
+  }
+  & > div + div {
+    margin-top: 2rem;
+  }
+`;
--- typing.d.ts
+++ typing.d.ts
@@ -6,3 +6,6 @@ declare module '*.svg' {
     const src: string;
     export default src;
   }
+
+  declare module '*.png';
+  declare module '*.jpg';
