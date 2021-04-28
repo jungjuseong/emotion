816aa5f8c89f10226ce17cdf71446476b575a5a4
순태겸 <taegyeom.soon@clbee.com>
2021-04-28 11:57:19 +0900

    Merge Fix

--- src/Icon/svg/index.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export { ReactComponent as exit } from './exit.svg';
-export { ReactComponent as heart } from './heart.svg';
-export { ReactComponent as pencil } from './pencil.svg';
